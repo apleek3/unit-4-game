b83eba1e47be5a0f96522b99340f6724c3711fa5
apleek3 <apleek3@gmail.com>
2018-06-24 19:28:03 -0700

    Random Number Generators are all good. Time to start generating crystals!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,20 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
-    console.log( "ready!" );
+// A $( document ).ready() block for starting to do things on load
+$(document).ready(function () {
+    var targetNumber = Math.round((Math.random()*((120-19))+19));
+    var crystalsTotal = 0;
+    
+    var crystals = []
+    while(crystals.length < 4) {
+        var randomCrystal = Math.round((Math.random()*(12-1)) + 1);
+        if (crystals.indexOf(randomCrystal) > -1) 
+        continue;
+        crystals[crystals.length] = randomCrystal;
+    }
+
+    console.log(targetNumber);
+    console.log(crystalsTotal);
+    console.log(crystals);
+    console.log("ready!");
 });
+
+
